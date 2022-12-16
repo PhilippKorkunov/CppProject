a053d18887a981ecafbfae02b94bb65cda43b41d
Philipp <filppkorkunov.1920@gmail.com>
2022-12-16 15:50:25 +0300

    upd: Teacher

--- Teacher.cpp
+++ Teacher.cpp
@@ -1,1 +1,24 @@
 #include "Teacher.h"
+#include<iostream>
+
+string Teacher::GetStatus()
+{
+	return "Преподаватель";
+}
+
+string Teacher::GetFullInfo()
+{
+	string out_str = "\n---------------------------\n";
+	out_str += "Статус: " + Teacher::GetStatus() + "\n";
+	out_str += Person::GetFullInfo();
+	out_str += "Предмет: " + subject + "\n";
+	out_str += "Стаж: " + std::to_string(experience) + "\n";
+	return out_str;
+}
+
+string Teacher::additionalFields[] = { "Предмет", "Стаж", };
+string* Teacher::allFields = Person::GetFullFeilds(Teacher::additionalFields, 2);
+
+int Teacher::GetSizeOfAdditionalFields() {
+	return sizeof(additionalFields) / sizeof(additionalFields[0]);
+}
